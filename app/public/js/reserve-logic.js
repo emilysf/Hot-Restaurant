525689da537b0cda14f5ef2f0381e4c695c65827
BFGriffith <benjaminforbesgriffith@gmail.com>
2016-06-25 12:50:47 -0400

    working on addReservation logic

--- app/public/js/reserve-logic.js
+++ app/public/js/reserve-logic.js
@@ -1,4 +1,37 @@
-// create new currentReservations - takes in JSON input
+var path = require('path');
+
+module.exports = function(app) {
+
+  $('#addBtn').on("click", function() {
+
+    var newReservation = {
+      name: $("#name").val().trim(),
+      phoneNumber: $("#number").val().trim(),
+      email: $("#email").val().trim(),
+      uniqueID: $("#id").val().trim()
+    };
+
+    var currentURL = window.location.origin;
+
+    $.post(currentURL + "/api/new", newReservation)
+      .done(function(data) {
+        console.log(data);
+				var newReservation = req.body;
+				newReservation.routeName = newReservation.name.replace(/\s+/g, '').toLowerCase()
+
+				console.log(newReservation);
+
+				currentReservations.push(newReservation);
+
+				res.json(newReservation);
+      })
+
+    return false;
+
+  });//END newReservation
+};//END module.exports
+
+/*
 app.post('/api/new', function(req, res){
 
 	var newReservation = req.body;
@@ -10,3 +43,4 @@ app.post('/api/new', function(req, res){
 
 	res.json(newReservation);
 })
+*/
--- server.js
+++ server.js
@@ -17,8 +17,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.text());
 app.use(bodyParser.json({type:'application/vnd.api+json'}));
 
-// Starts the server to begin listening
-// =============================================================
+// starts the server to begin listening
 app.listen(PORT, function(){
 	console.log('App listening on PORT ' + PORT);
 })
